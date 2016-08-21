78ef50ace32fc93a5177eed3051d40c88085aa64
Alexander.M <sancezz2006@gmail.com>
2016-08-21 22:37:48 +0500

    пробуем v-bind:class

--- index2.php
+++ index2.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <title>VueJS</title>
     <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.min.js"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div id="app">
     <ul>
-        <li v-for="car in cars">{{ car }}</li>
+        <li v-for="car in cars" v-bind:class="{ 'strike' : car.status }"> {{ car.name }}</li>
     </ul>
 </div>
 <script>
@@ -17,10 +18,23 @@
         data:{
             //создадим массив с марками авто
             cars:[
-                'toyota',
-                'opel',
-                'mazda',
-                'kia',
+                {
+                    name:'toyota',
+                    status: false
+                },
+                {
+                    name:'opel',
+                    status: true
+                },
+                {
+                    name:'mazda',
+                    status: false
+                },
+                {
+                    name:'kia',
+                    status: false
+                }
+
             ]
         }
     })
