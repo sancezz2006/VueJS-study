907db77a911bc108757a8a8d95545db514134886
Alexander.M <sancezz2006@gmail.com>
2016-08-23 11:36:06 +0500

    Делаем сайдбар, используя v-show и transition

--- index4.php
+++ index4.php
@@ -4,13 +4,20 @@
     <meta charset="UTF-8">
     <title>VueJS</title>
     <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.min.js"></script>
-    <link rel="stylesheet" href="style.css">
+
+    <link rel="stylesheet" href="style4.css">
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 </head>
 <body>
 <div id="app">
-    <button v-on:click="show = !show">Press</button>
-    <div class="index4_rectangle" v-if="show"></div>
-    <div v-else>Пусто</div>
+    <header>
+        <button v-on:click="show = !show">
+            <i class="ion-navicon"></i>
+        </button>
+    </header>
+
+    <aside v-show="show" transition="slide"></aside>
+
 
 </div>
 
